040e96fd2c01beeb2bcd227f68d777753180cf51
Nanda Kishor Vinod <nandakishorvinod2020@gmail.com>
2024-06-14 23:41:31 +0530

    Setting up the basic backend routes

---
+++ backend/src/index.ts
@@ -0,0 +1,90 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
+
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+    JWT_SECRET:string
+	}
+}>();
+
+
+app.post('api/v1/user/signup',async (c)=>{
+  const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body=await c.req.json();
+  try{
+    const user=await prisma.user.create({
+      data:{
+        email: body.email,
+        password: body.password,
+      }
+    })
+    const jwt=await sign({id:user.id},c.env.JWT_SECRET);
+    return c.json({jwt});
+  }catch(e){
+    return c.status(403);
+  }
+})
+
+
+
+app.post('api/v1/user/signin',async (c)=>{
+  const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body=await c.req.json();
+  const valid=await prisma.user.findUnique({
+    where:{
+      email:body.email,
+      password:body.password
+    }
+  })
+  if(valid){
+    const jwt=await sign({id:valid.id},c.env.JWT_SECRET);
+    return c.json({
+      jwt:jwt
+    })
+  }
+  else{
+    c.status(403);
+    return c.json({
+      message:'Invalid Credentials'
+    })
+  }
+
+  return c.text('signup route');
+})
+
+
+
+app.post('/api/v1/blog',(c)=>{
+  return c.text('blog route');
+})
+
+
+
+app.put('/api/v1/blog',(c)=>{
+  return c.text('blog signin');
+})
+
+
+
+app.get('/api/v1/blog/:id',(c)=>{
+        const id = c.req.param('id')
+	      console.log(id);
+        return  c.text('get blog route')
+})
+
+
+
+app.get('/api/v1/blog/bulk',(c)=>{
+      return c.text('bulk route'); 
+})
+
+
+
+export default app;
