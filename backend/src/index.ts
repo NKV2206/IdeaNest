4065be8fd0dfbb3bdbe8b52dafe1166b461a6e84
Nanda Kishor Vinod <nandakishorvinod2020@gmail.com>
2024-06-18 19:17:09 +0530

    Made minor changes to the backend routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import { Hono } from 'hono'
 import user from './routes/user';
 import blog from './routes/blog';
-
+import { cors } from 'hono/cors';
 const app = new Hono<{
   Bindings: {
       DATABASE_URL: string;
       JWT_SECRET: string;
   }
 }>();
+app.use('/*',cors());
 app.route('/api/v1/user', user)
 app.route('/api/v1/blog', blog)
 
